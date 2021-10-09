cd03d99d37731df5fc45b0984b076b5b04547d00
unknown <mihirmahajani25@gmail.com>
2021-10-09 23:29:49 +0530

    Added filter options for history

--- History/views.py
+++ History/views.py
@@ -13,6 +13,35 @@ def getHistoryPage(request):
     currUserHistory=allUserHistory.filter(uname=name)
     print("User history is: ",currUserHistory)
 
+    disease_name_filter=request.GET.get("disease_name_filter")
+    date_filter=request.GET.get("date_filter")
+    date_criteria=request.GET.get("date")
+    medicine_name_filter=request.GET.get("medicine_name_filter")
+    outcome_filter=request.GET.get("outcome_filter")
+
+    # Name filter
+    if disease_name_filter!='' and disease_name_filter is not None:
+        print("Disease Name Filter On")
+        currUserHistory=currUserHistory.filter(infection__icontains=disease_name_filter)
+
+    # Date filter
+    if date_filter!='' and date_filter is not None:
+        print("Date Filter On")
+        if date_criteria=='before':
+            currUserHistory=currUserHistory.filter(start_date__lt=date_filter)
+        if date_criteria=='after':
+            currUserHistory=currUserHistory.filter(start_date__gt=date_filter)
+    
+    # Medicine Name filter
+    if medicine_name_filter!='' and medicine_name_filter is not None:
+        print("Medicine Name Filter On")
+        currUserHistory=currUserHistory.filter(medicine__icontains=medicine_name_filter)
+    
+    # Outcome filter
+    if outcome_filter!='' and outcome_filter is not None:
+        print("Outcome Filter On")
+        currUserHistory=currUserHistory.filter(outcome=outcome_filter)
+
     # if not allUserHistory:
     #     print("empty")
     #     return render(request,"History/index.html")
